44ddc8853f442f215d11775ae808da64dfe9c4ff
Christine Antonio <cjoy.antonio@gmail.com>
2020-11-23 18:43:01 -0600

    adding files

---
+++ index.js
@@ -0,0 +1,38 @@
+function getQuantity() {
+  let enteredNum = $('#dog-quantity').val();
+  return enteredNum;
+}
+
+function getDogImages(quantity) {
+  console.log('Fetching your dog images');
+   fetch (`https://dog.ceo/api/breeds/image/random/${getQuantity()}`)
+   .then(response => response.json())
+   .then(responseJson => displayResults(responseJson))
+   .catch(error => alert('Something went wrong.Try again later.'))
+}
+
+// displaysResults to the DOM
+function displayResults(responseJson) {
+  console.log(responseJson);
+  let index = $('#dog-quantity').val();
+  let dogHtml = "";
+  for (let i=0; i < index; i++) {
+    dogHtml+=`<br><img src="${responseJson.message[i]}" class="results-img">`;
+  }
+  $('section h2').replaceWith(`<h2>You got ${getQuantity()} dogs!</h2>`);
+  $('div.response-images').html(dogHtml);
+  $('.results').removeClass('hidden');
+}
+
+// listens for user to submit form
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    getDogImages();
+  });
+}
+
+$(function() {
+  console.log('App loaded! Waiting for submit!');
+  watchForm();
+});
